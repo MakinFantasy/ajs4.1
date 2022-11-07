abdb57e09ff7e9b528faa3125f7c13120cd60fd8
MakinFantasy <beingp0z1t1v3@gmail.com>
2022-11-07 13:12:51 +0300

    finished task 1

---
+++ src/js/__test__/healthStatusChecker.test.js
@@ -0,0 +1,12 @@
+import healthStatusChecker from "../healthStatusChecker";
+
+test("test for health status checker func", () => {
+    const healthy = healthStatusChecker({name: 'Mage', health: 95})
+    expect(healthy).toBe('healthy')
+
+    const wounded = healthStatusChecker({name: 'Mage', health: 37})
+    expect(wounded).toBe('wounded')
+
+    const critical = healthStatusChecker({name: 'Mage', health: 5})
+    expect(critical).toBe('critical')
+})
---
+++ src/js/app.js
@@ -0,0 +1,3 @@
+import healthStatusChecker from "./healthStatusChecker";
+
+console.log(healthStatusChecker({name: 'Mage', health: 90}))
